f64d8a3a70a5014f8de8ba2d4e00ccdd4f17da38
Saša Barišić <cartman300@net.hr>
2015-09-02 03:35:38 +0200

    Project files

---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using RuntimePInvoke;
+
+namespace Test {
+	class Program {
+		static void Main(string[] args) {
+			Console.Title = "Test";
+
+			Console.WriteLine("Done!");
+			Console.ReadLine();
+		}
+	}
+}
